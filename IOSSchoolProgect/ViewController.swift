f1272e863a310360091eb4143ce1eef470ee088b
Max Vologin <>
2022-04-15 02:55:22 +0700

    Added text fields requirements

--- IOSSchoolProgect/ViewController.swift
+++ IOSSchoolProgect/ViewController.swift
@@ -9,8 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var tapGestureRecognizer: UITapGestureRecognizer!
+    
+    var inputTextFields: [UITextField] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        loginTextField.delegate = self
+        passwordTextField.delegate = self
+        inputTextFields = [loginTextField, passwordTextField]
+        
+        tapGestureRecognizer.addTarget(self, action: #selector(hideKeyboard))
     }
+    
+    @objc func hideKeyboard() {
+        view.endEditing(true)
+    }
+}
+
+extension ViewController: UITextFieldDelegate {
+        func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            if inputTextFields.last == textField {
+                textField.resignFirstResponder()
+                return true
+            }
+            if let index = inputTextFields.firstIndex (where: {
+                inputTextField in
+                inputTextField == textField
+            }) {
+                inputTextFields[index+1].becomeFirstResponder()
+            }
+            return true
+        }
+
 }
