65766c72b6d0323cf0c34f18ccefa79536688a5c
Max Vologin <>
2022-04-25 16:32:06 +0700

    Added ProfileTableViewController.

---
+++ IOSSchoolProgect/ProfileTableViewController.swift
@@ -0,0 +1,53 @@
+//
+//  UserLoginTableViewController.swift
+//  IOSSchoolProgect
+//
+//  Created by maxvologin on 25.04.2022.
+//
+
+import UIKit
+
+class ProfileTableViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        registerCells()
+    }
+    
+    func registerCells() {
+        registerCell(identifire: UserLoginTableViewCell.className)
+        registerCell(identifire: RegistrationDateTableViewCell.className)
+        registerCell(identifire: ProfileColorTableViewCell.className)
+    }
+    
+    func registerCell(identifire: String) {
+        let nib = UINib(nibName: identifire, bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: identifire)
+    }
+}
+
+extension ProfileTableViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row == 0,
+           let cell = tableView.dequeueReusableCell(withIdentifier: UserLoginTableViewCell.className) as? UserLoginTableViewCell {
+            return cell
+        }
+        if indexPath.row == 1,
+           let cell = tableView.dequeueReusableCell(withIdentifier: RegistrationDateTableViewCell.className) as? RegistrationDateTableViewCell {
+            return cell
+        }
+        if indexPath.row == 2,
+           let cell = tableView.dequeueReusableCell(withIdentifier: ProfileColorTableViewCell.className) as? ProfileColorTableViewCell {
+            return cell
+        }
+        return UITableViewCell()
+    }
+}
---
+++ IOSSchoolProgect/Rows/ProfileColorTableViewCell/ProfileColorTableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileColorTableViewCell.swift
+//  IOSSchoolProgect
+//
+//  Created by maxvologin on 25.04.2022.
+//
+
+import UIKit
+
+class ProfileColorTableViewCell: UITableViewCell {
+    
+}
