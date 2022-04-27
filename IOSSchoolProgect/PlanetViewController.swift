669a67e0b7f68b4af1714d8e9515d8f7b4679460
Max Vologin <>
2022-04-28 00:44:06 +0700

    Added transitions between ViewControllers without storyboard.

---
+++ IOSSchoolProgect/PlanetViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PlanetViewController.swift
+//  IOSSchoolProgect
+//
+//  Created by maxvologin on 27.04.2022.
+//
+
+import UIKit
+
+class PlanetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- IOSSchoolProgect/TabBarController.swift
+++ IOSSchoolProgect/TabBarController.swift
@@ -8,4 +8,51 @@
 import UIKit
 
 class TabBarController: UITabBarController {
+    
+    enum TabBarItem: Int {
+        case planet
+        case profile
+        
+        var title: String {
+            switch self {
+            case .planet:
+                return "Выбор планеты"
+            case .profile:
+                return "Профиль"
+            }
+        }
+        
+        var iconName: String {
+            switch self {
+            case .planet:
+                return "planet"
+            case .profile:
+                return "contact"
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTabBar()
+    }
+    
+    func setupTabBar() {
+        let tabBarItems: [TabBarItem] = [.planet, .profile]
+        viewControllers = tabBarItems.enumerated().map { (index, tabBarItem) in
+            switch tabBarItem {
+            case .planet:
+                guard let planetViewController = storyboard?.instantiateViewController(withIdentifier: PlanetViewController.className) as? PlanetViewController else { return UIViewController() }
+                planetViewController.title = tabBarItems[index].title
+                planetViewController.tabBarItem.title = tabBarItems[index].title
+                planetViewController.tabBarItem.image = UIImage(named: tabBarItems[index].iconName)
+                return UINavigationController(rootViewController: planetViewController)
+            case .profile:
+                guard let profileViewController = storyboard?.instantiateViewController(withIdentifier: ProfileViewController.className) as? ProfileViewController else { return UIViewController() }
+                profileViewController.tabBarItem.title = tabBarItems[index].title
+                profileViewController.tabBarItem.image = UIImage(named: tabBarItems[index].iconName)
+                return UINavigationController(rootViewController: profileViewController)
+            }
+        }
+    }
 }
