d3da3be420494da2967ce128363c3b9400965f61
Max Vologin <>
2022-04-28 16:01:01 +0700

    Added setting photo to profile.

--- IOSSchoolProgect/ProfileViewController.swift
+++ IOSSchoolProgect/ProfileViewController.swift
@@ -10,12 +10,30 @@ import UIKit
 class ProfileViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
-        
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet var tapGestureRecognizerForPhotoImageView: UITapGestureRecognizer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.dataSource = self
         navigationController?.setNavigationBarHidden(true, animated: false)
+        tapGestureRecognizerForPhotoImageView.addTarget(self, action: #selector(alertImage))
+        tableView.dataSource = self
         registerCells()
+    }
+    
+    @objc func alertImage() {
+        let alert = UIAlertController(title: nil, message: "Хотите выбрать новое изображение", preferredStyle: .alert)
+        let photo = UIAlertAction(title: "Выбрать", style: .default) {_ in
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.allowsEditing = true
+            imagePicker.sourceType = .photoLibrary
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+        let cancel = UIAlertAction(title: "Отмена", style: .cancel, handler: nil)
+        alert.addAction(photo)
+        alert.addAction(cancel)
+        self.present(alert, animated: true, completion: nil)
     }
     
     func registerCells() {
@@ -50,5 +68,15 @@ extension ProfileViewController: UITableViewDataSource {
             return cell
         }
         return UITableViewCell()
+    }
+}
+
+extension ProfileViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        photoImageView.image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage
+        photoImageView.contentMode = .scaleAspectFill
+        photoImageView.layer.frame = .init(x: 5, y: 5, width: 164, height: 164)
+        photoImageView.layer.cornerRadius = 82
+        dismiss(animated: true, completion: nil)
     }
 }
